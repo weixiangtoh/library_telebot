8c285333c636ff6611b39c80d6bbc38b519760bc
weixiangtoh <weixiangtoh982@gmail.com>
2020-08-29 23:30:22 +0800

    Changing dynamo from web to worker

--- main.py
+++ main.py
@@ -58,6 +58,6 @@ def webhook():
     return "!", 200
 
 bot.polling()
-# server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 # if __name__ == "__main__":
+    # server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
